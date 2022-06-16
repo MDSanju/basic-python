d8c24e35e644446564075690d32dc0c6a8122d56
MD Sanju <mdsanju387@gmail.com>
2022-06-17 02:11:11 +0600

    math related library functions

---
+++ library-function.py
@@ -0,0 +1,23 @@
+from math import *
+
+# Math related library functions
+
+max_res = max(20, 10)
+min_res = min(10, 20, 30)
+abs_res = abs(-4)
+pow_res = pow(2, 3)
+sqrt_res = sqrt(25)
+round_res = round(3.2)
+
+
+print("Min Number: ", max_res)
+print("Max Number: ", min_res)
+print("Abs Number: ", abs_res)
+print("Pow Number: ", pow_res)
+print("Sqrt Number: ", sqrt_res)
+print("Round Number: ", round_res)
+
+
+# Floor & Ceil function
+print(floor(3.7))
+print(ceil(3.7))
