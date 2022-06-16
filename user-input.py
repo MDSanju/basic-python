5ae6f263aa06b228e04840c0d782bd06e5d7a03c
MD Sanju <mdsanju387@gmail.com>
2022-06-16 12:10:19 +0600

    getting user input

---
+++ user-input.py
@@ -0,0 +1,10 @@
+print("Getting User's Input!")
+
+# Getting user's input
+name = input("Write your name: ")
+age = input("Put your age: ")
+country = input("Mention your country: ")
+
+print("Name : " + name)
+print("Age : " + age)
+print("Country : " + country)
