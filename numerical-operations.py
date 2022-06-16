9307854f90d7e365f88068788c075d6f3a12ed37
MD Sanju <mdsanju387@gmail.com>
2022-06-16 11:55:08 +0600

    numerical operations

--- numerical-operations.py
+++ numerical-operations.py
@@ -1,2 +1,18 @@
 sum = 4 + 3
 print(sum)
+
+# Basic numerical operations
+a = 18
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+# Floor number
+print(a // b)
+
+# Uses of "to the power"
+print(4 ** 3)
