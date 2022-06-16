968981aef7c75071d6d6d08f2bb1bbdc7798891b
MD Sanju <mdsanju387@gmail.com>
2022-06-16 10:56:19 +0600

    Initial commit

---
+++ program01.py
@@ -0,0 +1,21 @@
+print("Getting Started Python!")
+
+# Getting Input
+num = input()
+
+# Output showing
+print("Output:", num)
+
+
+# Uses of the Backslash (\) Characters
+
+# For a new line
+print("MD Sanju \nPhone: +8801786649178")
+
+# For a tab(4) space
+print("Hello \t World!")
+
+# For putting quotation both side of text
+print("\"My name: Sanju\"")
+
+
