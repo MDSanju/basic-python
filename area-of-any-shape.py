05ff281d59b575f65e15a29b40b4ecce2e06386f
MD Sanju <mdsanju387@gmail.com>
2022-06-17 01:48:10 +0600

    calculate plane shape of area

---
+++ area-of-any-shape.py
@@ -0,0 +1,20 @@
+# Calculate plane shape of area
+
+# Triangle
+base = input("Enter Base = ")
+height = input("Enter Height = ")
+
+tr_base = float(base)
+tr_height = float(height)
+
+area = 0.5 * tr_base * tr_height
+
+print("Area of a Triangle", area)
+
+
+# Area of a circle
+radius = float(input("Enter Radius = "))
+
+area_of_radius = 3.1416 * radius * radius
+
+print("Area of a Circle : ", area_of_radius)
