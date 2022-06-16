a8393fc7efe0c4d47e1a215c1e07ab7b24b4013a
MD Sanju <mdsanju387@gmail.com>
2022-06-17 01:26:41 +0600

    type casting

---
+++ type-casting.py
@@ -0,0 +1,13 @@
+# Type Casting in Python
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+converted_num1 = int(num1)
+converted_num2 = int(num2)
+
+sum = converted_num1 + converted_num2
+print("The sum is:", sum)
+
+
+subtraction = converted_num1 - converted_num2
+print("The subtraction is:", subtraction)
