e5a104aab731d0c424e36acf8809ff9f2b183953
MD Sanju <mdsanju387@gmail.com>
2022-06-17 08:06:38 +0600

    type fun, string formate & end=" "

---
+++ formatted-string-&-tupe-func.py
@@ -0,0 +1,28 @@
+# Type function
+num_int = 20
+num_type = type(num_int)
+print(num_type)
+
+num_float = 15.7
+float_type = type(num_float)
+print(float_type)
+
+str = "Hello world"
+str_type = type(str)
+print(str_type)
+
+print(type(True))
+
+
+# Formatted string
+value1 = 20
+value2 = 30
+
+print(f"The sum: {value1} + {value2} = {value1 + value2}!")
+
+
+# End for line concatenate
+
+print("Hello World!", end=" ")
+print("It's me Sanju and I am learning Python!", end=" ")
+print("That's cool!")
